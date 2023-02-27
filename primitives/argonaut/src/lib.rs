1ecf974278658ba7772c47dcf2743a5d84f1a669
Justin Frevert <justinfrevert@gmail.com>
2023-02-27 00:44:55 -0500

    forgot example code

--- primitives/argonaut/src/lib.rs
+++ primitives/argonaut/src/lib.rs
@@ -1,16 +1,16 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use sp_runtime_interface::runtime_interface;
-// use pqcrypto_falcon::{falcon1024_verify_detached_signature, falcon512_verify_detached_signature};
+use pqcrypto_falcon::falcon512_verify_detached_signature;
 
 #[runtime_interface]
 pub trait ArgonautPrimitive {
     fn falcon_verify(signature: &[u8], msg: &[u8], pk: &[u8]) {
-        // use pqcrypto_traits::sign::DetachedSignature;
-        // use pqcrypto_traits::sign::PublicKey;
+        use pqcrypto_traits::sign::DetachedSignature;
+        use pqcrypto_traits::sign::PublicKey;
 
-        // let detached_sig = &DetachedSignature::from_bytes(signature).unwrap();
-        // let pk = &PublicKey::from_bytes(pk).unwrap();
-        // falcon512_verify_detached_signature(detached_sig, msg, pk);
+        let detached_sig = &DetachedSignature::from_bytes(signature).unwrap();
+        let pk = &PublicKey::from_bytes(pk).unwrap();
+        falcon512_verify_detached_signature(detached_sig, msg, pk);
     }
 }
